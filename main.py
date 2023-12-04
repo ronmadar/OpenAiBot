2d400cf54c6a2d74fc2ac12a7f120da147ed25c3
Ron Madar <Ronlevi892@gmail.com>
2023-12-04 10:59:12 +0200

    first commit all project

---
+++ main.py
@@ -0,0 +1,23 @@
+import openai
+
+
+openai.api_key = "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"
+
+def chat_with_gpt(promt):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role" : "user" , "content" : promt}]
+    )
+
+    return response['choices'][0]['message']['content'].strip()
+
+
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit" , "bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot" , response)
